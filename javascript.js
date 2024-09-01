15a67ebd0c2ef8e0c481c632b230bdfd268e418b
bigmaniac <1matik.m@gmail.com>
2024-09-01 19:09:06 +0200

    Add logic to the game
    - add computer choice logic
    - add human choice logic

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+const choices = ['rock', 'paper', 'scissors'];
+
+function computerChoice() {
+    let computerChoiceResult = Math.floor(Math.random() * 3);
+    return choices[computerChoiceResult];
+}
+console.log(computerChoice())
+
+function humanChoice() {
+    while (true) {
+        let humanChoiceResult = parseInt(prompt('Please select your figure: (0 - rock, 1 - paper, 2 - scissors)'));
+        if (humanChoiceResult === 0 || humanChoiceResult === 1 || humanChoiceResult === 2) {
+            return choices[humanChoiceResult]
+        } else {
+            alert('Invalid choice. Please try again.')
+        }
+    }
+}
+
+console.log(humanChoice())
