e1cdffa9c0e797f27236121b422941f05e2c7b59
bigmaniac <1matik.m@gmail.com>
2024-09-01 21:20:43 +0200

    Add logic for the game

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ["rock", "paper", "scissors"];
 
 let humanScore = 0;
 let computerScore = 0;
@@ -10,45 +10,48 @@ function computerChoice() {
 
 function humanChoice() {
     while (true) {
-        let humanChoiceResult = parseInt(prompt('Please select your figure: (0 - rock, 1 - paper, 2 - scissors)'));
+        let humanChoiceResult = parseInt(prompt("Please select your figure: (0 - rock, 1 - paper, 2 - scissors)"));
         if (humanChoiceResult === 0 || humanChoiceResult === 1 || humanChoiceResult === 2) {
             return choices[humanChoiceResult];
         } else {
-            alert('Invalid choice. Please try again.');
+            alert("Invalid choice. Please try again.");
         }
     }
 }
 
 function playRound (humanChoiceResult, computerChoiceResult) {
-    if (humanChoiceResult === computerChoiceResult) {
-        console.log("It's a tie.");
-    } else if (humanChoiceResult === 'rock' && computerChoiceResult === 'paper') {
-        computerScore ++;
-        console.log(`Paper beats rock. You loose.`);
-    } else if (humanChoiceResult === 'rock' && computerChoiceResult === 'scissors') {
-        humanScore ++;
-        console.log('Rock beats scissors. You win.');
-    } else if (humanChoiceResult === 'paper' && computerChoiceResult === 'rock') {
-        humanScore++;
-        console.log(`Paper beats rock. You win.`);
-    } else if (humanChoiceResult === 'paper' && computerChoiceResult === 'scissors') {
-        computerScore++;
-        console.log(`Scissors beats paper. You loose.`);
-    } else if (humanChoiceResult === 'scissors' && computerChoiceResult === 'rock') {
-        computerScore++;
-        console.log(`Rock beats scissors. You loose.`);
-    } else if (humanChoiceResult === 'scissors' && computerChoiceResult === 'paper') {
-        humanScore++;
-        console.log(`Scissors beats paper. You win.`);
+    console.log(`Your choice: ${humanChoiceResult}.`);
+    console.log(`Computer choice: ${computerChoiceResult}.`);
+    if (humanChoiceResult === "rock" && computerChoiceResult === "scissors" ||
+        humanChoiceResult === "paper" && computerChoiceResult === "rock" ||
+        humanChoiceResult === "scissors" && computerChoiceResult === "paper") {
+            humanScore++;
+            console.log(`${humanChoiceResult.substring(0, 1).toUpperCase() + humanChoiceResult.substring(1)} beats ${computerChoiceResult}. You won the round.`);
+    } else if (computerChoiceResult === "rock" && humanChoiceResult === "scissors" ||
+        computerChoiceResult === "paper" && humanChoiceResult === "rock" ||
+        computerChoiceResult === "scissors" && humanChoiceResult === "paper") {
+            computerScore++;
+            console.log(`${computerChoiceResult.substring(0, 1).toUpperCase() + computerChoiceResult.substring(1)} beats ${computerChoiceResult}. You loose the round.`);
+    } else {
+        console.log(`${computerChoiceResult.substring(0, 1).toUpperCase() + computerChoiceResult.substring(1)} is equal to ${computerChoiceResult}. It's a draw round.`);
     }
-    console.log(`Your choice: ${humanChoiceResult}`);
-    console.log(`Computer choice: ${computerChoiceResult}`);
-    console.log(`Current score - You ${humanScore} | Computer: ${computerScore}`);
+    console.log(`Current score: You ${humanScore} | Computer: ${computerScore}`);
 }
 
-const humanChoiceResult = humanChoice();
-const computerChoiceResult = computerChoice();
+function playGame(num) {
+    for (let i = 1; i <= num; i++) {
+        const humanChoiceResult = humanChoice();
+        const computerChoiceResult = computerChoice();
+        playRound(humanChoiceResult, computerChoiceResult);
+    }
+}
 
+playGame(5);
 
-playRound(humanChoiceResult, computerChoiceResult)
-
+if (humanScore > computerScore) {
+    console.log(`You won the game.`);
+} else if (humanScore < computerScore) {
+    console.log(`You lost the game.`)
+} else {
+    console.log(`It's a draw.`)
+}
